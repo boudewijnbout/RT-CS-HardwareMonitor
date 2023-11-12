616a3923e523979ca46f134941e85ceedad3861d
Boudewijn Bout <boudewijn_bout@outlook.com>
2023-11-12 19:54:31 +0100

    Add project files.

---
+++ HardwareMonitor/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HardwareMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, EventArgs e)
+        {
+            // Get CPU information
+            string cpuName = GetCPUName();
+
+            // Display CPU information in the GUI
+            lblCPUName.Text = $"CPU Name: {cpuName}";
+        }
+
+        private string GetCPUName()
+        {
+            string cpuName = "";
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    cpuName = obj["Name"].ToString();
+                    break; // Assuming there's only one processor
+                }
+            }
+            return cpuName;
+        }
+    }
+}
